b621bde183822bfc20606bab008b26c4a5291cb0
Jonathan Arnett <jonarnett90@gmail.com>
2023-02-03 17:03:44 -0500

    Add tests

--- test/saturn_test.exs
+++ test/saturn_test.exs
@@ -1,4 +1,92 @@
 defmodule SaturnTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
   doctest Saturn
+
+  @default_measurements %{total_time: 123_456_789}
+  @default_metadata %{
+    query: "SELECT * FROM users;",
+    stacktrace: [{Saturn, :fake, 1, [file: "saturn.ex", line: 5]}]
+  }
+
+  setup_all do
+    :ok =
+      :telemetry.attach(
+        "saturn-aggregator",
+        [:test, :repo, :query],
+        &Saturn.handle_query/4,
+        nil
+      )
+  end
+
+  setup do
+    Saturn.clear()
+    Saturn.enable()
+  end
+
+  describe "disable" do
+    test "does not track queries when disabled" do
+      Saturn.disable()
+
+      send_query()
+
+      %{queries: queries} = :sys.get_state(Saturn.Aggregator)
+
+      assert queries == %{}
+    end
+  end
+
+  describe "enable" do
+    test "tracks queries when enabled" do
+      Saturn.enable()
+
+      send_query()
+
+      %{queries: queries} = :sys.get_state(Saturn.Aggregator)
+
+      assert Enum.count(queries) == 1
+    end
+  end
+
+  describe "report" do
+    setup do
+      send_query()
+      send_query()
+
+      send_query(
+        measurements: %{@default_measurements | total_time: 123_456_789_000},
+        metadata: %{@default_metadata | query: "SELECT * FROM users WHERE id = 5;"}
+      )
+    end
+
+    test "with no arguments orders queries by count" do
+      report = Saturn.report()
+
+      assert {:ok,
+              [
+                {%{query: "SELECT * FROM users;"}, 2},
+                {%{query: "SELECT * FROM users WHERE id = 5;"}, 1}
+              ]} = report
+    end
+
+    test "can order by queries by time" do
+      report = Saturn.report(:time)
+
+      assert {:ok,
+              [
+                {%{query: "SELECT * FROM users WHERE id = 5;"}, _},
+                {%{query: "SELECT * FROM users;"}, _}
+              ]} = report
+    end
+  end
+
+  defp send_query(overrides \\ []) do
+    measurements = Keyword.get(overrides, :measurements, @default_measurements)
+    metadata = Keyword.get(overrides, :metadata, @default_metadata)
+
+    :telemetry.execute(
+      [:test, :repo, :query],
+      measurements,
+      metadata
+    )
+  end
 end
